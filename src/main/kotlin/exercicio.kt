0da96eaecf90317f0fa844af7a2d9fcb285c313e
alinessantana <aline.sousa.santana10@gmail.com>
2022-11-28 18:46:48 -0300

    Initial commit

---
+++ src/main/kotlin/exercicio.kt
@@ -0,0 +1,12 @@
+//Escreva um programa para ajudar a empresa XPTO calcular o bônus dos funcionários que receberão
+//no final do ano. Os bônus são classificados por cargo\\
+
+//salário dos cargos : \\
+//Gerentes recebem R$2.000,00\\
+//Coordenadores recebem $1.500,00\\
+//Engenheiros de Software recebem R$ 1.000,00\\
+//Estagiários recebem R$ 500,00\\
+
+
+
+
